324b92928e6e93f00fd9ca02ea2c94e400a9e622
EC2 Default User <ec2-user@ip-172-31-25-198.ap-northeast-1.compute.internal>
2019-02-14 07:12:40 +0000

    make index.html

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(read("pages/index.txt"));
+});
+
+app.get('/test', function (req, res) {
+  res.send('Hello test!');
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
+
+function read(filePath) {
+  var fs = require('fs');
+  var content = new String();
+  content = fs.readFileSync(filePath, 'utf8');
+  console.log("content:" + content);
+  return content;
+};
