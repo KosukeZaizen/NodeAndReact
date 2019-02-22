310e0e0df4b6f13d422f174d7683d047f159f724
Kosuke Zaizen <kosukezaizen@yahoo.co.jp>
2019-02-22 20:33:21 +0900

    make htmls folder

--- myapp/app.js
+++ myapp/app.js
@@ -1,28 +1,67 @@
-var express = require('express');
-var app = express();
+console.log("hello");
 
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
+//htmlsフォルダ削除
+var rimraf = require('rimraf');
+rimraf("views", function (err) {
+    if(err){
+        console.error(err);
+        process.exit(1);
+    }else{
+        console.log('views folder removed!');
+
+        //viewsフォルダをコピーして、新たなhtmlsフォルダを作成
+        var fsExtra = require('fs-extra');
+        fsExtra.copy('htmls', 'views', {
+            clobber: true,
+            dereference: false,
+            filter: function (element) {
+                return true;
+            },
+            preserveTimestamps: false
+        }, function (err) {
+          if(err){
+            console.error(err);
+            process.exit(1);
+          }else{
+            console.log('views folder was recreated!');
+
+             //viewsフォルダ内の.ejsファイルの拡張子を.htmlに変換
+             let fs = require('fs');
+             fs.readdir('./views', function(err, files){
+               if (err) throw err;
+               console.log("change from " + files);
+               for(let htmlFile of files){
+                   var oldPath = './views/' + htmlFile;
+                   var newPath = './views/' + htmlFile.replace(".html", ".ejs");
+                   fs.rename(oldPath, newPath, function (err) {
+                     if (err) throw err
+                     console.log(oldPath + ':  Successfully renamed - AKA moved!')
+                   })
+               }
+             });
+          }
+      });
+    }
+});
+
+
+//サーバー起動
+var express = require('express'),
+  app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render("index");
 });
 
-app.get('/test', function (req, res) {
-  res.send('Hello test!');
+app.get('/:name', function (req, res) {
+  res.render(req.params.name);
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-})
+});
 
 
-/*
-function read(filePath) {
-  var fs = require('fs');
-  var content = new String();
-  content = fs.readFileSync(filePath, 'utf8');
-  console.log("content:" + content);
-  return content;
-};
-*/
